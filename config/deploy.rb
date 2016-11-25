460ac1af00e1c64ed710dfd9db49cd5f2c0a0bb4
hanyong37 <chenxi0809@gmail.com>
2016-11-26 03:23:08 +0800

    change to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,10 @@ set :domain, 'xinjudian.net'
 set :deploy_to, '/var/www/newpoint'
 set :repository, 'git://github.com/hanyong37/newpoint.git'
 set :branch, 'master'
+set :forward_agent, true
+set :app_path, lambda { "#{deploy_to}/#{current_path}" }
+set :stage, 'production'
+set :rvm_path, '/home/deploy/.rvm/bin/rvm'
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
@@ -66,37 +70,74 @@ task :setup => :environment do
   end
 end
 
+#desc "Deploys the current version to the server."
+#task :deploy => :environment do
+#  to :before_hook do
+#    # Put things to run locally before ssh
+#  end
+#  deploy do
+#    # Put things that will set up an empty directory into a fully set-up
+#    # instance of your project.
+#    invoke :'git:clone'
+#    invoke :'deploy:link_shared_paths'
+#    invoke :'bundle:install'
+#    invoke :'rails:db_migrate'
+#    invoke :'rails:assets_precompile'
+#    invoke :'deploy:cleanup'
+#
+#    to :launch do
+#      #queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+#      #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+#
+#      queue %[cd #{deploy_to}/#{current_path}]
+#      if `cat #{deploy_to}/tmp/server.pid` != ''
+#        queue "cat #{deploy_to}/tmp/server.pid | xargs kill -s TERM"
+#      end
+#      queue 'bin/rails s -d -e production --pid `pwd`/../../tmp/server.pid'
+#    end
+#  end
+#end
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   to :before_hook do
-    # Put things to run locally before ssh
   end
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
+    queue! "cd #{app_path} & RAILS_ENV=#{stage} bundle exec rake db:create"
     invoke :'rails:db_migrate'
+    queue! "cd #{app_path} & RAILS_ENV=#{stage} bundle exec rake db:seed"
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
+    invoke :'puma:restart'
 
     to :launch do
-      #queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-
-      queue %[cd #{deploy_to}/#{current_path}]
-      if `cat #{deploy_to}/tmp/server.pid` != ''
-        queue "cat #{deploy_to}/tmp/server.pid | xargs kill -s TERM"
-      end
-      queue 'bin/rails s -d -e production --pid `pwd`/../../tmp/server.pid'
     end
   end
 end
-
 # For help in making your deploy script, see the Mina documentation:
 #
 #  - http://nadarei.co/mina
 #  - http://nadarei.co/mina/tasks
 #  - http://nadarei.co/mina/settings
 #  - http://nadarei.co/mina/helpers
+namespace :puma do
+  desc "Start the application"
+  task :start do
+    queue 'echo "-----> Start Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh start", :pty => false
+  end
+
+  desc "Stop the application"
+  task :stop do
+    queue 'echo "-----> Stop Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh stop"
+  end
+
+  desc "Restart the application"
+  task :restart do
+    queue 'echo "-----> Restart Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh restart"
+  end
+end
